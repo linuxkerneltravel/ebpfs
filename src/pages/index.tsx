f1668c68ff94353cb72165727e5dc729cd271b4a
hanbings <hanbings@hanbings.io>
2023-07-21 02:06:58 +0800

    fix: home page and login logic.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import Navbar from "@/pages/components/Navbar";
+import Button from "@/pages/components/Button";
+import Avatar from "@/pages/components/Avatar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
-  return (
-      <h1>eBPF Hub</h1>
-  )
+    return (
+        <main style={{ backgroundImage: `url("https://i.im.ge/2023/07/17/5jrzzK.F0ci1uZakAAukOL.jpg")` }}
+              className={`flex min-h-screen flex-col ${inter.className} bg-fixed bg-cover bg-center`}>
+            <Navbar />
+            <div className="min-h-screen w-full backdrop-blur-2xl flex flex-col gap-2 items-center justify-center">
+                <img src="favicon.ico"  alt="" style={{ height: "72px" }}/>
+                <p className="text-3xl text-gray-600">欢迎使用 eBPF Hub</p>
+                <p className="text-xl text-gray-600">搜索、探索 eBPF 程序</p>
+                <div className="mt-2">
+                    <Button icon="icons8-github.svg" href="" text="使用 Github 登录"/>
+                </div>
+            </div>
+        </main>
+    )
 }
