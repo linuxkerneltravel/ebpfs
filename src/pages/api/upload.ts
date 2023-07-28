991cb1cd98998a8c935710f362e3e60fbd6ec02d
hanbings <hanbings@hanbings.io>
2023-07-28 19:46:23 +0800

    feat: algolia upload service.

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -4,6 +4,7 @@ import CacheService from "@/services/cache";
 import {Token} from "@/common/token";
 import {Repository} from "@/common/repository";
 import DatabaseService from "@/services/database";
+import SearchService from "@/services/search";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<{}>) {
     if (req.method === 'POST') {
@@ -44,6 +45,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         )
 
         await repositories.createRepository(repo);
+
+        const algoliaApplicationID = process.env.ALGOLIA_APPLICATION_ID;
+        const algoliaAPIKey = process.env.ALGOLIA_API_KEY;
+
+        if (!algoliaApplicationID) {
+            res.status(500).json({error: 'ALGOLIA_APPLICATION_ID is not set'});
+            return;
+        }
+
+        if (!algoliaAPIKey) {
+            res.status(500).json({error: 'ALGOLIA_API_KEY is not set'});
+            return;
+        }
+
+        const search = new SearchService(algoliaApplicationID, algoliaAPIKey);
+
+        await search.upload({
+            id: repo.id,
+            url: repo.repository,
+            organization: repo.organization,
+            project: repo.project,
+            readme: repo.readme,
+            author: repo.author,
+            tags: repo.tags
+        });
 
         res.status(200).json({})
     } else res.status(400).json(new Message(400, 'request method not match.', null));
