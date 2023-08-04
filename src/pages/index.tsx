0ee7fcb1243eeb034dd5e43732ca4a16a9ee8480
hanbings <hanbings@hanbings.io>
2023-08-04 11:07:45 +0800

    fix: fix the problem of getting undefined environment variables.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,15 @@ import {State} from "@/pages/_app";
 
 const inter = Inter({subsets: ['latin']})
 
-function getGithubAuthorizeUrl() {
-    const githubClientID = process.env.GITHUB_OAUTH_CLIENT_ID;
-    const state = crypto.randomUUID();
-    const baseUrl = process.env.BASE_URL;
-
-    return `https://github.com/login/oauth/authorize?client_id=${githubClientID}&state=${state}&redirect_uri=${baseUrl}/login`;
-}
-
 export default function Home() {
+    function getGithubAuthorizeUrl() {
+        const githubClientID = process.env.GITHUB_OAUTH_CLIENT_ID;
+        const state = crypto.randomUUID();
+        const baseUrl = process.env.BASE_URL;
+
+        return `https://github.com/login/oauth/authorize?client_id=${githubClientID}&state=${state}&redirect_uri=${baseUrl}/login`;
+    }
+
     return (
         <main style={{backgroundImage: `url("https://i.im.ge/2023/07/17/5jrzzK.F0ci1uZakAAukOL.jpg")`}}
               className={`flex min-h-screen flex-col ${inter.className} bg-fixed bg-cover bg-center`}>
