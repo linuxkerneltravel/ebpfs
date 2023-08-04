c1898980f072affaf04cc9279013d17cd631658f
hanbings <hanbings@hanbings.io>
2023-08-04 15:39:34 +0800

    fix: unable to store state normally.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,42 @@ import {Account} from "@/common/account";
 import {withRouter} from "next/router";
 
 export class State {
-    // TODO: 从 localStorage 中读取 token 和 account
-    // TODO: 清理 localStorage 中的 token 和 account
+    // 从 localStorage 中读取 token 和 account
+    constructor() {
+        if (typeof window !== "undefined") {
+            const token = localStorage.getItem("token");
+            const account = localStorage.getItem("account");
+            if (token !== null && account !== null) {
+                State.token = JSON.parse(token);
+                State.account = JSON.parse(account);
+            }
+        }
+    }
+
+    // 清理 localStorage 中的 token 和 account
+    public static clear() {
+        if (typeof window !== "undefined") {
+            localStorage.removeItem("token");
+            localStorage.removeItem("account");
+        }
+
+        State.token = null;
+        State.account = null;
+    }
 
     public static token: Token | null = null;
     public static account: Account | null = null;
+
+    // 写入 localStorage
+    public static save(token: Token, account: Account) {
+        State.token = token;
+        State.account = account;
+
+        if (typeof window !== "undefined") {
+            localStorage.setItem("token", JSON.stringify(State.token));
+            localStorage.setItem("account", JSON.stringify(State.account));
+        }
+    }
 
     public static isLogin(): boolean {
         return State.token !== undefined && State.account !== undefined;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <main style={{backgroundImage: `url("https://i.im.ge/2023/07/17/5jrzzK.F0ci1uZakAAukOL.jpg")`}}
               className={`flex min-h-screen flex-col ${inter.className} bg-fixed bg-cover bg-center`}>
-            <Navbar/>
+            <Navbar src={State.account?.avatar ? State.account.avatar : `https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png` }/>
             <div className="min-h-screen w-full backdrop-blur-2xl flex flex-col gap-2 items-center justify-center">
                 <img src="favicon.ico" alt="" style={{height: "72px"}}/>
                 <p className="text-3xl text-gray-600">欢迎使用 eBPF Hub</p>
