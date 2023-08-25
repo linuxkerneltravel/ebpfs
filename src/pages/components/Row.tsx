170a58100ee792223737e6d692a7b26e68573d4e
hanbings <hanbings@hanbings.io>
2023-08-25 17:54:09 +0800

    fix: repair hydration response.

--- src/pages/components/Row.tsx
+++ src/pages/components/Row.tsx
@@ -30,7 +30,7 @@ class Row extends Component<RowProps> {
                 <p className="text-gray-600 text-sm">{this.props.text}</p>
                 <div className="flex flex-row flex-wrap gap-2">
                     {
-                        this.props.tags && this.props.tags.map((value, index) => {
+                        this.props.tags && this.props.tags.map && this.props.tags.map((value, index) => {
                             return (
                                 <div key={index} className="bg-gray-200 rounded-full px-2 py-1 text-gray-600 text-sm">
                                     {value}
