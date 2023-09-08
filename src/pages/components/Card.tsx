230b90178cf1b74a672d6982f1107f1c3d34500a
hanbings <hanbings@hanbings.io>
2023-09-08 17:34:01 +0800

    style: changed card component style.

--- src/pages/components/Card.tsx
+++ src/pages/components/Card.tsx
@@ -1,22 +1,24 @@
 import {Component} from "react";
+import Link from "next/link";
 
 interface CardProps {
     project: string;
     org: string;
+    url: string;
 }
 
 export default class Card extends Component<CardProps, {}> {
     render() {
         return (
             <div className="flex flex-col items-center justify-center">
-                <div className="flex flex-row shadow-lg items-center gap-4 p-4"
+                <a href={this.props.url} className="flex flex-row shadow-lg items-center gap-4 p-4"
                      style={{height: "108px", width: "250px"}}>
                     <img src="favicon.ico" alt="" style={{height: "42px"}} width="42"/>
                     <div className="flex flex-col justify-center">
                         <p className="text-lg text-gray-900">{this.props.project}</p>
                         <p className="text-sm text-gray-600">{`${this.props.org} / ${this.props.project}`}</p>
                     </div>
-                </div>
+                </a>
             </div>
         );
     }
